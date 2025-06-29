19815cbdb63a7e330e5b519d607715b53015d6fb
Vika <vg.dev31@gmail.com>
2025-06-29 18:44:37 +0400

    Initial Commit

---
+++ ProfilePageTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfilePageTask
+//
+//  Created by Vika on 29.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
